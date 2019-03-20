a781120051138ed711e906179d45539b1e0309e0
Mayki235 <ahmat.t@outlook.com>
2019-03-20 21:07:36 +0100

    update

---
+++ Cinéma/Page1.php
@@ -0,0 +1,206 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.css" >
+    <link rel="stylesheet" href="css/Style.css">
+    <title>Voir film</title>
+  </head>
+
+  <body>
+    
+ <div class="couleure">
+    <header class ="entete" >
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <a class="navbar-brand" href="#">ACCUEIL</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+       <div class="collapse navbar-collapse" id="navbarSupportedContent">
+         <ul class="navbar-nav mr-auto">
+            <li class="nav-item dropdown">
+              <a class="nav-link " href="page2.php">
+                HORAIRES
+              </a>
+            </li>
+
+             <li class="nav-item">
+              <a class="nav-link " href="page2.php">INSCRIPTION</a>
+            </li>
+              <li class="nav-item">
+              <a class="nav-link " href="#" tabindex="-1" aria-disabled="true">SE CONNECTER</a>
+            </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">SUIVI</button>
+        </form>
+  </div>
+</nav>
+    <!-- image logo !-->
+    <nav class="navbar  navbar-dark bg-dark">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col">
+               <a class="navbar-brand" href="Page1.html">
+              <img  src="logo.jpg" width="100" height="100" alt="">
+             </a>
+            </div>
+          </div>
+
+        </div>
+   </nav>
+
+   <!-- image ciné
+   <div class"row" >
+    <img src="cinema.jpg">
+   </div>
+ !-->
+  </header>
+</div>
+
+
+  <br>
+  <p ><h2 class="une">A LA UNE</h2></p>
+     <div align="center">
+     <button type="button" class="btn btn-danger"><span class="plop">RESERVER</span> </button>
+   </div>
+
+  <br>
+    <div class="container">
+         <div class=" row ">
+            <div class="col-3">
+              <img src="bb.jpg" class="bb">
+              <p class=" ligne1"> Breaking Bad</p>
+            </div>
+            <div class="col-3">
+              <img src="peaky.jpg" class="bb">
+              <p class=" ligne1"> Peaky Blinders</p>
+            </div>
+            <div class="col-3">
+              <img src="got.jpg" class="bb">
+              <p class=" ligne1"> Game Of Thrones</p>
+            </div>
+            <div class="col-3">
+              <img src="marvel.jpg" class="bb">
+              <p class=" ligne1"> Captain Marvel</p>
+            </div>
+         </div>
+    </div>
+
+  <br>
+  <br>
+
+   <p ><h2 class="une">A propos de notre cinéma</h2></p>
+   <br>
+    <div align="center">
+      <div class="row">
+        <div class="col-3" >
+          <p align="center">
+           <section>
+             Situé au cœur de la ville
+             , le cinéma Pathé Evreux vous accueille 365 jours par an pour vivre vos émotions intensément ! Avant ou après votre séance
+             , prolongez votre expérience dans les espaces conviviaux à votre disposition en profitant de notre large offre de confiseries et de boissons.
+          </section>
+          </p>
+        </div>
+        <div class="col-3">
+          <p class="place">
+            <h2> 10</h2>
+            salles
+          </p>
+        </div>
+        <div class="col-3">
+         <p class="place">
+            <h2> 1542</h2>
+            places
+          </p>
+        </div>
+      </div>
+   </div>
+
+
+  <div >
+   <br>
+
+   <p><h2 class="plan">RECEVER DE BONS PLANS</h2></p>
+
+     <div class="container">
+      <div class="row">
+
+       <div class="col-4">
+       <p><h4 class="actualite">Pour ne rien manquer de l'actualité UGC, recevez par email les bons plans et les offres UGC.</h4></p>
+       <p class="petitepolice">
+        Votre adresse email sera utilisée pour vous transmettre les emails « Bons plans UGC » et à des fins statistiques
+       , et ce uniquement par les services internes d'UGC CINÉ CITÉ et les sociétés en lien avec UGC CINÉ CITÉ pour la réalisation de la prestation. La communication de votre adresse email est facultative,
+       à tout moment vous pouvez fermer cette fenêtre et continuer votre navigation sur le site ugc.fr si vous ne souhaitez pas recevoir les « Bons plans UGC ». Conformément à la loi n°78-17 du 6 janvier 1978 «Informatique et Libertés », modifiée en 2004, vous disposez d'un droit d'accès, de rectification,
+       de suppression des données vous concernant et d'opposition à son traitement en adressant votre demande par mail à serviceclient@ugc.fr. Votre adresse email sera conservée pour une durée de 48h à compter de votre demande de désabonnement aux « Bons plans UGC ».
+       </p>
+       </div>
+     <div class="col-4">
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+       <form class="form-inline">
+
+         <label class="sr-only" for="inlineFormInputGroupUsername2">Mail</label>
+         <div class="input-group mb-2 mr-sm-2">
+           <div class="input-group-prepend">
+             <div class="input-group-text">@</div>
+           </div>
+           <input type="text" class="form-control" id="inlineFormInputGroupUsername2" placeholder="Mail">
+         </div>
+         <button type="button" class="btn btn-danger btn-sm">Je m'inscrit</button>
+     </div>
+    </div>
+     </div>
+  </div>
+
+
+ <footer class="foo">
+   <p class="up_footer" >Contact</p>
+   <div class="container">
+     <div class="row">
+       <div class="col-4">
+         <ul>
+          <li>welcome to OMEGA</li>
+          <li>welcome to OMEGA</li>
+          <li>welcome to OMEGA</li>
+         </ul>
+       </div>
+       <div class="col-4">
+        <ul>
+          <li>welcome to OMEGA</li>
+          <li>welcome to OMEGA</li>
+          <li>welcome to OMEGA</li>
+         </ul>
+       </div>
+       <div class="col-4">
+        <ul>
+          <li>welcome to OMEGA</li>
+          <li>welcome to OMEGA</li>
+          <li>welcome to OMEGA</li>
+         </ul>
+       </div>
+     </div>
+    </div>
+ </footer>
+
+
+
+  </body>
+
+</html>
---
+++ Cinéma/page2.php
@@ -0,0 +1,160 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="css/bootstrap.css" >
+  <link rel="stylesheet" href="css/Style.css">
+  <title>INSCRIPTION</title>
+</head>
+<body>
+  <div class="couleure">
+     <header class ="entete" >
+     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+       <a class="navbar-brand" href="Page1.php">ACCUEIL</a>
+       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+         <span class="navbar-toggler-icon"></span>
+       </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+             <li class="nav-item dropdown">
+               <a class="nav-link " href="page2.php">
+                 HORAIRES
+               </a>
+             </li>
+
+             <li class="nav-item">
+              <a class="nav-link " href="page2.php">INSCRIPTION</a>
+            </li>
+              <li class="nav-item">
+              <a class="nav-link " href="#" tabindex="-1" aria-disabled="true">SE CONNECTER</a>
+            </li>
+         </ul>
+         <form class="form-inline my-2 my-lg-0">
+           <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+           <button class="btn btn-outline-success my-2 my-sm-0" type="submit">SUIVI</button>
+         </form>
+   </div>
+ </nav>
+     <!-- image logo !-->
+     <nav class="navbar  navbar-dark bg-dark">
+         <div class="container-fluid">
+           <div class="row">
+             <div class="col">
+                <a class="navbar-brand" href="Page1.php">
+               <img  src="logo.jpg" width="100" height="100" alt="">
+              </a>
+             </div>
+           </div>
+
+         </div>
+    </nav>
+    <br>
+    <br>
+    <br>
+    <section>
+        <div class="row">
+            <div class="col-2">
+
+            </div>
+            <div class="col-8">
+              <div class="cadr">
+              <form class="needs-validation" novalidate>
+                      <div class="form-row">
+                        <div class="col-md-4 mb-3">
+                          <label for="validationTooltip01"> <strong>First name</strong></label>
+                          <input type="text" class="form-control" id="validationTooltip01" placeholder="First name" value="Mark" required>
+                          <div class="valid-tooltip">
+                            Looks good!
+                          </div>
+                        </div>
+                        <div class="col-md-4 mb-3">
+                          <label for="validationTooltip02"><strong>Last name</strong></label>
+                          <input type="text" class="form-control" id="validationTooltip02" placeholder="Last name" value="Otto" required>
+                          <div class="valid-tooltip">
+                            Looks good!
+                          </div>
+                        </div>
+                        <div class="col-md-4 mb-3">
+                          <label for="validationTooltipUsername"><strong>Username</strong></label>
+                          <div class="input-group">
+                            <div class="input-group-prepend">
+                              <span class="input-group-text" id="validationTooltipUsernamePrepend">@</span>
+                            </div>
+                            <input type="text" class="form-control" id="validationTooltipUsername" placeholder="Username" aria-describedby="validationTooltipUsernamePrepend" required>
+                            <div class="invalid-tooltip">
+                              Please choose a unique and valid username.
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="form-row">
+                        <div class="col-md-6 mb-3">
+                          <label for="validationTooltip03"><strong>City</strong></label>
+                          <input type="text" class="form-control" id="validationTooltip03" placeholder="City" required>
+                          <div class="invalid-tooltip">
+                            Please provide a valid city.
+                          </div>
+                        </div>
+                        <div class="col-md-3 mb-3">
+                          <label for="validationTooltip04"><strong>State</strong></label>
+                          <input type="text" class="form-control" id="validationTooltip04" placeholder="State" required>
+                          <div class="invalid-tooltip">
+                            Please provide a valid state.
+                          </div>
+                        </div>
+                        <div class="col-md-3 mb-3">
+                          <label for="validationTooltip05"><strong>Zip</strong></label>
+                          <input type="text" class="form-control" id="validationTooltip05" placeholder="Zip" required>
+                          <div class="invalid-tooltip">
+                            Please provide a valid zip.
+                          </div>
+                        </div>
+                      </div>
+                      <button class="btn btn-primary" type="submit">Submit form</button>
+                      </form>
+            </div>
+            </div>
+            <div class="col-2">
+
+            </div>
+        </div>
+    </section>
+    <br>
+    <br>
+    <br>
+    <br>
+
+    <footer class="foo">
+      <p class="up_footer" >Contact</p>
+      <div class="container">
+        <div class="row">
+          <div class="col-4">
+            <ul>
+             <li>welcome to OMEGA</li>
+             <li>welcome to OMEGA</li>
+             <li>welcome to OMEGA</li>
+            </ul>
+          </div>
+          <div class="col-4">
+           <ul>
+             <li>welcome to OMEGA</li>
+             <li>welcome to OMEGA</li>
+             <li>welcome to OMEGA</li>
+            </ul>
+          </div>
+          <div class="col-4">
+           <ul>
+             <li>welcome to OMEGA</li>
+             <li>welcome to OMEGA</li>
+             <li>welcome to OMEGA</li>
+            </ul>
+          </div>
+        </div>
+       </div>
+    </footer>
+</body>
+</html>
